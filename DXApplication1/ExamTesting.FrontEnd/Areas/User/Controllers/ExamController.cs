66c900d694122bbded1a516e90250349158f2f75
PATHOMPHOB SUNANTA <pathomphob_sun@cmu.ac.th>
2019-07-30 06:07:02 +0700

    Exam Test Select Choice

--- DXApplication1/ExamTesting.FrontEnd/Areas/User/Controllers/ExamController.cs
+++ DXApplication1/ExamTesting.FrontEnd/Areas/User/Controllers/ExamController.cs
@@ -7,10 +7,12 @@ using DevExtreme.AspNet.Data;
 using DevExtreme.AspNet.Mvc;
 using Microsoft.AspNetCore.Mvc;
 using ExamTesting.DAL;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ExamTesting.FrontEnd.Areas.Users.Controllers
 {
     [Area("User")]
+    //[Authorize(Roles = "User")]
     public class ExamController : Controller
     {
         private readonly ExamTestingDbContext _db;
@@ -32,7 +34,7 @@ namespace ExamTesting.FrontEnd.Areas.Users.Controllers
 
 
         [HttpGet]
-        public object GetQuestion(DataSourceLoadOptions loadOptions, Guid id)
+        public object GetExamQuestion(DataSourceLoadOptions loadOptions, Guid id)
         {
             return DataSourceLoader.Load(_db.QuestionExams.Where(u => u.ExamId == id), loadOptions);
         }
@@ -43,5 +45,37 @@ namespace ExamTesting.FrontEnd.Areas.Users.Controllers
             return DataSourceLoader.Load(_db.Choices.Where(u => u.QuestionId == id), loadOptions);
         }
 
+
+
+
+
+        public IActionResult QuestionAnswer(Guid id)
+        {
+            var _exams = _db.QuestionExams.First(a => a.QuestionId == id);
+            return View(_exams);
+        }
+
+
+        [HttpGet]
+        public IActionResult Test(Guid id)
+        {
+            var _UserExams = _db.UserExams.First(a => a.ExamId == id);
+            _UserExams.StartExam();
+
+            _db.SaveChanges();
+            return View(_UserExams);
+        }
+
+        [HttpPost, ActionName("Test")]
+        public IActionResult TestPost(Guid id, Guid questionAnswer)
+        {
+
+            var _questionAnswer = _db.UserExamQuestions.First(a => a.UserExam.ExamId == id);
+            _questionAnswer.SelectChoiceId = questionAnswer;
+
+            _db.SaveChanges();
+
+            return RedirectToAction("Test");
+        }
     }
 }
